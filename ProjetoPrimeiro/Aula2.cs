c89a8eed737607c1069b5398ee7df866ae0657d1
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-02-18 22:19:07 -0300

    Criei o arquivo Aula2.cs

---
+++ ProjetoPrimeiro/Aula2.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ProjetoPrimeiro 
+{
+    class Aula2 
+    {
+        static void main(string[] args)
+        {
+            string nome = "Jackson";
+            int idade = 34;
+
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+        }
+
+    }
+
+}
