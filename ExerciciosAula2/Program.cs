d6b9a5b70e9618ad7ce0d470ef55fd4d9394df28
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-02-21 11:06:44 -0300

    Update em Program.cs
    Resposta do Exercício01

--- ExerciciosAula2/Program.cs
+++ ExerciciosAula2/Program.cs
@@ -2,11 +2,30 @@
 
 namespace ExerciciosAula2
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exercicio01();
+        }
+        static void Exercicio01()
+        {
+            string produto1 = "Computador 1";
+            string produto2 = "Mesa de escritório";
+
+            int idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double preco2Total = preco2 - 0.5;
+            double media = 53.234567;
+
+            Console.WriteLine("Produtos:\n" + produto1 +  ", cujo o preço é $ " + preco1 +",00\n" + produto2 + ", cujo o preço é $ " + preco2Total +",20\n" +"Registro: " + idade + " anos de idade," + " código " + codigo + " e gênero " + genero + "\n" +"Media com oito casas decimais: " + media +"00\n");
+            Console.WriteLine(String.Format("Arredondado (três casas decimais): {0:F}", 53.234567));
+        
+
         }
     }
 }
