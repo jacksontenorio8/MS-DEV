134a2e944ba315ed1f6244a95237c21db3ddb54d
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-02-28 13:59:20 -0300

    Update
    Entrada e saída de dados.

--- Aula3/Program.cs
+++ Aula3/Program.cs
@@ -10,7 +10,8 @@ namespace Aula3
             //POR EXEMPLO: Abaixo vai ser executado apenas o operadorLogico.
             
             //OperadorLogico();
-            EntradaDeDados();
+            //EntradaDeDados();
+            SaidaDeDados();
         }
 
         static void OperadorLogico()
@@ -60,7 +61,41 @@ namespace Aula3
         }
         static void EntradaDeDados()
         {
-            //
+            //ENTRADA DE DADOS.
+
+            Console.WriteLine("Digite seu nome: ");
+
+            // Console.ReadLine(); -> O PROGRAMA VAI LER O QUE FOI DIGITADO NO TECLADO E AGUARDAR A TECLA ENTER.
+
+            // leitura da linha que o usuário digitou.
+            string usuario = Console.ReadLine(); //Jackson
+            Console.WriteLine("O nome informado foi: " + usuario);
+        }
+        static void SaidaDeDados()
+        {
+            //SAÍDA DE DADOS.
+
+            int idade = 34;
+            double saldo = 279.97;
+            string nome = "Jackson";
+            string nome1 = "Tenorio";
+
+            //PLACEHOUDER - lugar para colocar a variável.
+            //{0} - posição.
+
+            // Jackson tem 34 anos e tem saldo igual a 279.97 reais.
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais.", nome, idade, saldo);
+
+            //interpolação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo + " reais.");
+
+            Console.WriteLine(saldo);
+
+            //convertendo int para string.
+            Console.WriteLine(saldo.ToString("F2")); //deixa com duas casas e arredonda para cima
+
+            //convertendo int para string.
+            Console.WriteLine(saldo.ToString("F3")); //deixa com três casas e arredonda para cima
         }
     }
 }
