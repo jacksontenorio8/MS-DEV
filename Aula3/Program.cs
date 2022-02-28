9ce08a94e9599826cfe9aee2cdf4490767273969
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-02-28 13:13:56 -0300

    Update
    Operadores Lógicos

--- Aula3/Program.cs
+++ Aula3/Program.cs
@@ -9,6 +9,58 @@ namespace Aula3
             //Para executar basta tirar o comentário da função desejadas
             //POR EXEMPLO: Abaixo vai ser executado apenas o operadorLogico.
             
+            //OperadorLogico();
+            EntradaDeDados();
+        }
+
+        static void OperadorLogico()
+        {
+            //OPERADOR LÓGICO
+
+            // CONDIÇÃO E = && 
+
+            // A PRIMEIRA CONDIÇÃO TEM QUE SER VERDADEIRA E A SEGUNDA TAMBÉM
+
+            bool analise = 2 > 3 && 4 != 5;
+
+            // NÃO (FALSE)
+
+            // TRUE - TRUE
+
+            bool analise2 = !(2 > 3) && 4 != 5;
+
+            //Console.WriteLine(analise);
+            //Console.WriteLine(analise2);
+
+            String nome = "Jackson";
+            String nome2 = "Tenorio";
+
+            //  nome JACKSON não é igual a Tenorio
+
+            bool nomeVerdade = (nome == "Jackson");
+           
+            Console.WriteLine(nomeVerdade); //TRUE
+
+            bool nomeVerdade1 = (nome2 == "Tenorio");
+
+            Console.WriteLine(nomeVerdade1); //TRUE
+
+            //&& -> todas as condições devem ser TRUE.
+            bool nomeVerdade2 = (nome == "Jackson" && nome == "Tenorio");
+            Console.WriteLine("Testar aqui: " + nomeVerdade2); //FALSE.
+
+            bool nomeVerdade3 = (nome != nome2);
+            Console.WriteLine(nomeVerdade3);
+
+            // || OU -> uma das condições deve ser TRUE.
+
+            bool nomeVerdade4 = (nome == "Jackson" || nome == "Tenorio");
+            Console.WriteLine(nomeVerdade4);
+
+        }
+        static void EntradaDeDados()
+        {
+            //
         }
     }
 }
