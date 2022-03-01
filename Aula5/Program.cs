89eb0228de9cdb5dcc0e437fc0b5c2be1f84111e
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-03-01 11:49:52 -0300

    Update em Aula5
    - Laço de repetição for
    - Array

--- Aula5/Program.cs
+++ Aula5/Program.cs
@@ -6,9 +6,18 @@ namespace Aula5
     {
         static void Main(string[] args)
         {
+            //For();
+            Array();
+        } 
+
+        static void For()
+        {
             //FOR
+            
             //EXIBIR NÚMEROS ATÉ 50.
+            
             //for TAB + TAB
+            
             //i = inicio da variavel. INDEX
 
             // i < 50 = condição para a quantidade de vezes que o bloco vai executar
@@ -19,7 +28,22 @@ namespace Aula5
             {
                 Console.WriteLine(i);
             }
-        } 
+        }
+
+        static void Array()
+        {
+            //ARRAY ("VETOR")
+            //posições [0], [1], [2], [3], [4], [5], [6]
+
+            int[] numeros_loteria = { 4, 8, 15, 16, 23, 42 };
+
+            Console.WriteLine("Posição 2: " + numeros_loteria[2]);
+
+            for(int i = 0; i < numeros_loteria.Length; i++)
+            {
+                Console.WriteLine(numeros_loteria[i]);
+            }
+        }
         
     }
 }
