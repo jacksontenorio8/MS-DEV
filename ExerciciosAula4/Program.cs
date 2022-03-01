10fe028e6aac858347cfe67efc51dc997449ff5d
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-02-28 21:39:32 -0300

    Update em ExerciciosAula4
    Resolução do exercício01

--- ExerciciosAula4/Program.cs
+++ ExerciciosAula4/Program.cs
@@ -6,7 +6,38 @@ namespace ExerciciosAula4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exercicio01();
+        }
+
+        static void Exercicio01()
+        {
+            /*
+            Crie um algoritmo no qual o usuário/aluno deverá informar informar seu nome e quatro notas. O programama deverá calcular sua média e informar se o aluno foi aprovado ou não. O número seis é o valor mínimo para aprovação.
+            */
+
+            int nota1, nota2, nota3, nota4, media;
+
+            Console.WriteLine("1ª nota: ");
+            nota1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2ª nota: ");
+            nota2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("3ª nota: ");
+            nota3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("4ª nota: ");
+            nota4 = Convert.ToInt32(Console.ReadLine());
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+            Console.WriteLine("A idade média do aluno é: " + media); 
+
+            if (media >= 6)
+            {
+                Console.WriteLine("Aluno Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Aluno Reprovado!");
+            }
+
+
         }
     }
 }
