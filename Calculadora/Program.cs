9b23ccf5af7646e777acd2bc8281e87a1a1af2c0
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-03-11 22:10:22 -0300

    Criei o projeto Calculadora.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0;
+            int numero2 = 0;
+            int total = 0;
+            string operacao = "";
+
+            Console.WriteLine("Programa que faz operações com dois números!");
+            numero1 = Teclado("Digite o 1º número: ");
+            numero2 = Teclado("Digite o 2º número: ");
+            Console.WriteLine("Informe a operação a ser realizada (adição, subtração, multiplicação e divisão): ");
+            operacao = Console.ReadLine;
+        }
+
+    }
+}
