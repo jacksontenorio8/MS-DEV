5f3c6b5aab0845fdfa1abacaeaf5ed2ad10581f3
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-03-13 07:59:40 -0300

    Projeto celular

---
+++ Celular/Celular.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Celular
+{
+    class Celular
+    {
+        public string modelo;
+        public string cor;
+        public string operadora;
+        
+        public void Ligar()
+        {
+            ConsultarCreditos();
+            DigitarNumero();
+        }
+
+        public void ConsultarCreditos()
+        {
+
+        }
+
+        public void DigitarNumero()
+        {
+            
+        }
+
+    }
+}
---
+++ Celular/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Celular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celular sansung = new Celular();
+        }
+    }
+
+    
+}
